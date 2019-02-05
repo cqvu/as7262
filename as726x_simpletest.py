b27e1cde87eb04bca3880743cc0debff42ae8de9
Chau Vu <chv004@ucsd.edu>
2019-02-05 09:48:25 -0800

    Added constant frequency sampling

--- as726x_simpletest.py
+++ as726x_simpletest.py
@@ -10,14 +10,9 @@ from PushingBox import PushingBox
 pbox = PushingBox()
 devid = 'vED428DFB43710F0'
 
+startTime = time.time()
 #maximum value for sensor reading
 max_val = 16000
-
-#max number of characters in each graph
-max_graph = 80
-
-def graph_map(x):
-    return min(int(x * max_graph / max_val), max_graph)
 
 # Initialize I2C bus and sensor.
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -29,6 +24,7 @@ while True:
     # Wait for data to be ready
     while not sensor.data_ready:
         time.sleep(.1)
+
     print('Violet: ' + str(sensor.violet))
     print('Blue: ' + str(sensor.blue))
     print('Green: ' + str(sensor.green))
@@ -37,17 +33,15 @@ while True:
     print('Red: ' + str(sensor.red))
     print("\n")
 
-    pbox.push(devid, sensorData=sensor.blue)
-    time.sleep(50)
+    with open('as7262_readings.txt', 'a+') as write_file:
+        write_file.write(str(sensor.blue))
 
+    try:
+        pbox.push(devid, sensorData=sensor.blue)
+    except:
+        print("Could not push to PushingBox. Check internet connection.")
 
-'''
-    #plot plot the data
-    print("\n")
-    print("V: " + graph_map(sensor.violet)*'=')
-    print("B: " + graph_map(sensor.blue)*'=')
-    print("G: " + graph_map(sensor.green)*'=')
-    print("Y: " + graph_map(sensor.yellow)*'=')
-    print("O: " + graph_map(sensor.orange)*'=')
-    print("R: " + graph_map(sensor.red)*'=')
-'''
+    while(time.time() - startTime <= 60):
+        time.sleep(1)
+    startTime = startTime + 60
+
