f7fd41d856f349bdd327d31fa0db3026c9119165
Chau Vu <chv004@ucsd.edu>
2019-03-01 16:21:09 -0800

    Added built-in LED blinking

--- AS7262_SD.ino
+++ AS7262_SD.ino
@@ -1,4 +1,4 @@
-/***************************************************************************
+/**************************************************************************
   This is a library for the Adafruit AS7262 6-Channel Visible Light Sensor
 
   This sketch reads the sensor
@@ -21,7 +21,9 @@
 #include <TimeLib.h>
 #include "Adafruit_AS726x.h"
 
-const int chipSelect = 10;
+const int chipSelect = 4;
+const unsigned long READ_PERIOD = 1000; //1 minute
+unsigned long time_now = 0;
 
 //create the object
 Adafruit_AS726x ams;
@@ -35,7 +37,8 @@ uint16_t sensorValues[AS726x_NUM_CHANNELS];
 void setup() {
   Serial.begin(9600);
   while(!Serial);
-  
+
+  Serial.println("Begin!");
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -44,20 +47,24 @@ void setup() {
     Serial.println("could not connect to sensor! Please check your wiring.");
     while(1);
   }
-
+  /*
   Serial.print("Initializing SD card...");
 
   // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
+  while (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
-    while (1);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1);
+    digitalWrite(LED_BUILTIN, LOW);
   }
   Serial.println("card initialized.");
+  */
 }
 
 void loop() {
-  
+  static unsigned long lastRead;
+  time_now = millis();
   //ams.drvOn(); //uncomment this if you want to use the driver LED for readings
   ams.startMeasurement(); //begin a measurement
   
@@ -79,26 +86,34 @@ void loop() {
   Serial.print(" Red: "); Serial.print(sensorValues[AS726x_RED]);
   Serial.println();
   Serial.println();
-*/
+  */
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   time_t timeStamp = now();
   //Serial.println(now());
   String dataString = String(timeStamp) + "," + String(sensorValues[AS726x_VIOLET]) + "," + String(sensorValues[AS726x_BLUE]) + 
-                      "," + String(sensorValues[AS726x_GREEN]) + + "," + String(sensorValues[AS726x_YELLOW]) + + "," + String(sensorValues[AS726x_ORANGE]) + "," + String(sensorValues[AS726x_RED]);
-
+                      "," + String(sensorValues[AS726x_GREEN]) + + "," + String(sensorValues[AS726x_YELLOW]) + + "," + 
+                      String(sensorValues[AS726x_ORANGE]) + "," + String(sensorValues[AS726x_RED]);
   Serial.println(dataString);
-  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   /*
   File dataFile = SD.open("data.txt", FILE_WRITE);
   // if the file is available, write to it:
   if (dataFile) {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     dataFile.println(dataString);
     dataFile.close();
     // print to the serial port too:
     Serial.println(dataString);
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   }
   // if the file isn't open, pop up an error:
   else {
     Serial.println("error opening datalog.txt");
   
-  */
+  }
+*/
+  while( millis() < time_now + READ_PERIOD) {
+    //wait
+  }
+  
 }
